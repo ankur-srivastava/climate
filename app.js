51f4d93142593ccb7f67290daf5500835085e696
ankur-srivastava <ankurlkw@gmail.com>
2018-06-09 15:08:10 +0530

    Add  Changes

--- app.js
+++ app.js
@@ -1,8 +1,37 @@
 const request = require('request');
+const yargs = require('yargs');
+
+//Using yargs to get user address from command line
+//Like $node app.js a Hill Ridge Springs Hyderabad
+//a is an alias for address
+
+const argv = yargs
+    .options({
+      a: {
+        demand: true,
+        alias: 'address',
+        describe: 'Address to fetch weather for',
+        //this attribute tells Yargs to parse address as a String
+        string: true
+      }
+    })
+    .help()
+    .argv;
+
+var input_address = encodeURIComponent(argv.a);
 
 request({
-  url: 'http://maps.googleapis.com/maps/api/geocode/json?address=Hyderabad',
+  url: 'http://maps.googleapis.com/maps/api/geocode/json?address='+input_address,
   json:true
 }, (error, response, body)=>{
-  console.log(JSON.stringify(body, undefined, 2));
+  if(error){
+      console.log(JSON.stringify(error, undefined, 2));
+  }else{
+      var address = 'Address: '+body.results[0].formatted_address;
+      var lat = 'Latitude: '+body.results[0].geometry.location.lat;
+      var lon = 'Longitude: '+body.results[0].geometry.location.lng;
+      console.log(address);
+      console.log(lat);
+      console.log(lon);
+  }
 });
